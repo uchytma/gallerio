eed89b3e46e044353f5563a386ef7a1bfe8efc6f
Matěj Uchytil <uchytma@gmail.com>
2022-09-21 18:46:40 +0200

    Rename dir Gallerio.Api.ViewModels to Gallerio.Api.Dtos

--- Gallerio.Api.Dtos/Multimedia.cs
+++ Gallerio.Api.Dtos/Multimedia.cs
@@ -7,8 +7,6 @@ using System.Threading.Tasks;
 namespace Gallerio.Api.Dtos
 {
     public record MultimediaItemDto(Guid Id, string PartialPath, DateTime CaptureDateTime);
-
     public record MultimediaItemTagsDto(List<string> Tags);
-
     public record AddTag(string Name);
 }
